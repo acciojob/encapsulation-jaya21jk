2cad8193e6772f7bbf7bb185684624e65e688f32
cjayakrishna21@gmail.com <cjayakrishna21@gmail.com>
2023-03-14 11:05:23 +0530

    Second update

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
-        obj.name = "accio";
-        System.out.println(obj.name);
+//        obj.name = "accio";
+//        System.out.println(obj.name);
 
         //  java: name has private access in com.driver.RWOnly
 
